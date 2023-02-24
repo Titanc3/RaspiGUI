18f99559a26cec59d5100c9c38bb7e58e403a693
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2023-02-24 14:41:15 -0500

    version 1.10

--- GUI.py
+++ GUI.py
@@ -6,24 +6,18 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-version = "v1.0.1" # IMPORTANT: UPDATE EVERY RELEASE
-
 packed = True
 first = True
 root = Tk()
+version = "1.10"
 root.title(" ")
 frame = Frame(root)
 frame.pack()
-icon = PhotoImage(file = r"./logo.png") # If we assume we're being ran from the directory we're installed in, we should find the logo in the samme directory.
-main = Label(frame, image = icon, height = "240")
-main.pack(side = TOP, pady = 10)
+icon = PhotoImage(file = r"/home/titance/Downloads/logo.png") #IMPORTANT, CHANGE THIS TO IMAGE LOCATION
+main = Label(frame, image = icon, height = "200")
 title = Label(frame, text = "RaspiPanel", font =(
   "URW Gothic", 45))
-title.pack(side = TOP)
-credits = Label(frame, text = version + "by Titanc3 and Killaship", font =("Quicksand", 20))
-credits.pack(side = TOP)
 times = Label(frame, text = str(t.asctime(t.localtime())), font =("Quicksand", 10))
-times.pack(side = TOP)
 v3_3 = "1 17".split(" ")
 v5 = "2 4".split(" ")
 GND = "6 9 14 20 25 30 34 39".split(" ")
@@ -51,6 +45,10 @@ gpio_trans = {7:4,
               40:21}
 
 def base():
+    global manual_gpio
+    global Version
+    global InitialTitle
+    global InitialLogo
     global times
     global title
     global gpio
@@ -61,7 +59,36 @@ def base():
     global packed
     global extr1
     global extr2
-    
+
+    def manual_gpio():
+        global gpio_trans
+        for number in gpio_trans:
+            GPIO.setup(number, GPIO.OUT)
+            GPIO.output(number, 0)
+
+    def logo_clean():
+        global packed
+        global Version
+        global times
+        global title
+        global main
+        global gpio
+        global media
+        global tools
+        global first
+        global InitialTitle
+        global InitialLogo
+        Version.destroy()
+        InitialTitle.destroy()
+        InitialLogo.destroy()
+        main.pack(side = TOP, pady = 10)
+        title.pack(side = TOP)
+        times.pack(side = TOP)
+        gpio.pack(side = RIGHT, fill = BOTH, padx = 2, pady = 2)
+        tools.pack(side = BOTTOM, fill = BOTH, padx = 2, pady = 2)
+        media.pack(side = TOP, fill = BOTH, padx = 2, pady = 2)
+        first = False
+
     def off(port):
         global media
         GPIO.output(int(port), 0)
@@ -93,7 +120,7 @@ def base():
             media.configure(text = "SPI", state = DISABLED, disabledforeground = "white", background = "grey")
         else:
             GPIO.setup(int(port), GPIO.OUT)
-            media.configure(text = "GPIO {gpio_trans[int(port)]}",state = NORMAL, foreground = "green")
+            media.configure(text = f"GPIO {gpio_trans[int(port)]}",state = NORMAL, foreground = "green")
             if GPIO.input((int(port))) == 0:
                 media.configure(background = "dark grey", command = lambda: on(port))
             else:
@@ -134,29 +161,25 @@ def base():
         gpio.configure(text = "    Back    ", command = gpio_back,activebackground = "black",activeforeground = "#bc1142",background = "#75a928")
         extr1.pack(side = BOTTOM, fill = BOTH, padx = 2, pady = 2)
         
-        
-        
     def mediacmd():
-        GPIO.cleanup()
+        manual_gpio()
         exit()
-    
     
     if packed == False:
         title.pack_forget()
         times.pack_forget()
-        credits.pack_forget()
         gpio.pack_forget()
         tools.pack_forget()
         media.pack_forget()
         main.pack(side = TOP)
         title.pack(side = TOP)
         times.pack(side = TOP)
-        credits.pack(side = TOP)
         gpio.pack(side = RIGHT, fill = BOTH, padx = 2, pady = 2)
         tools.pack(side = BOTTOM, fill = BOTH, padx = 2, pady = 2)
         media.pack(side = TOP, fill = BOTH, padx = 2, pady = 2)
         packed = True
     if first == True:
+        
         gpio = Button(frame, text = "GPIO PINS", command = gpiocmd, height = 2,activebackground = "black",activeforeground = "#bc1142", background = "#75a928",foreground = "white", disabledforeground = "white")
         
         tools = Button(frame, text = "Tools", command = toolcmd, activebackground = "black",activeforeground = "#bc1142",background = "#bc1142",foreground = "white", disabledforeground = "white")
@@ -165,13 +188,16 @@ def base():
         
         extr1 = Spinbox(frame, from_ = 1, to = 40, command = boxcmd, background = "#bc1142",foreground = "white", justify = CENTER, width = 2, wrap = True, buttonbackground = "#75a928")
 
+        InitialLogo = Label(frame, text = "\nTitanc3", font = (
+        "URW Gothic", 80))
+        InitialLogo.pack(side = TOP)
+        Version = Label(frame, text = "Version "+version, font = ("Quicksand", 20))
+        Version.pack(side = BOTTOM)
+        InitialTitle = Label(frame, text = "<Studios>", font = ("Quicksand", 45))
+        InitialTitle.pack(side = BOTTOM)
         
+        frame.after(4000, logo_clean)
         
-        gpio.pack(side = RIGHT, fill = BOTH, padx = 2, pady = 2)
-        tools.pack(side = BOTTOM, fill = BOTH, padx = 2, pady = 2)
-        media.pack(side = TOP, fill = BOTH, padx = 2, pady = 2)
-        
-        first = False
     
     else:
         gpio.configure(text = "GPIO PINS", state = NORMAL, command = gpiocmd, height = 2,activebackground = "black",activeforeground = "#bc1142", background = "#75a928",foreground = "white", disabledforeground = "white")
@@ -179,12 +205,8 @@ def base():
         media.configure(text = "Exit", state = NORMAL, command = mediacmd, activebackground = "black",activeforeground = "#bc1142",background = "#bc1142",foreground = "white", disabledforeground = "white")
 try:
     base()
-    
 except KeyboardInterrupt:
     print("Program Interrupted, ending...")
-
+    manual_gpio()
 except:
-    print("Eror, Review Code")
-#finally: Messed up on my system, it might've been that I ran it wrong (out of an IDE, until I did,) but the OS's kernel will most likely clean up this sort of thing once the program is killed, so this isn't required.
-#    GPIO.cleanup()
-
+    print("Error, Review Code")
